e4d120329973643bc3791df08fd776eabba094a4
unknown <tungnguyen22013@gmail.com>
2021-06-22 20:51:27 +0700

    file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,38 @@
-$(document).ready( function() {
-    $("li.dropdown-submenu > a").on("click", function(){
-    $("li.dropdown-submenu").removeClass("open");
-    if($(this).parent("li.dropdown-submenu").hasClass("in-use")){
-    $(this).parents("li.dropdown-submenu").addClass("open");
-    $(this).parent("li.dropdown-submenu").removeClass("open");
-    $(this).parent("li.dropdown-submenu").removeClass("in-use");
-    $(this).siblings('ul.dropdown-menu').children("li.dropdown-submenu").removeClass("open");
-    $(this).siblings('ul.dropdown-menu').children("li.dropdown-submenu").removeClass("in-use");
-    } else {
-    $(this).parents("li.dropdown-submenu").addClass("open");
-    $(this).parents("li.dropdown-submenu").addClass("in-use");
+
+function openMobie() {
+    var headerPopup=document.querySelector('.header__popup');
+    headerPopup.classList.add('open__popup');
+}
+    
+
+function closeMobie(){
+    var headerPopup=document.querySelector('.header__popup');
+    headerPopup.classList.remove('open__popup');
+}
+
+  function accBtnClick($e) {
+    var accBtn = document.getElementsByClassName("item__button");
+    var accPanel = document.getElementsByClassName("item__panel");
+    for (var i = 0; i < accBtn.length; i++) {
+      accBtn[i].onclick = function () {
+        var setClasses = !this.classList.contains("active");
+        setClass(accBtn, "active", "remove");
+        setClass(accPanel, "show", "remove");
+        this.nextElementSibling.style.maxHeight = null;
+        if (setClasses) {
+          this.classList.toggle("active");
+          this.nextElementSibling.classList.toggle("show");
+          this.nextElementSibling.style.maxHeight =
+            this.nextElementSibling.scrollHeight + "px";
+          this.parentNode.style.boxShadow = "#00000059 0 6px 15px -13px";
+        }
+      };
     }
-    return false;
-    });
-    });
-
-    $(document).ready(function(){
-
-        $("ul li:has(ul)>a ").addClass("dropdown-toggle");
-        
-        $("ul li:has(ul)>a ").attr("data-toggle","dropdown");
-        
-        $("ul li:has(ul) ul ").addClass("dropdown-menu multi-level");
-        
-        $("ul ul li:has(ul) ").addClass("dropdown-submenu");
-        
-        });
+  }
+  
+  function setClass(els, className, fnName) {
+    for (var i = 0; i < els.length; i++) {
+      els[i].classList[fnName](className);
+      els[i].parentNode.style.boxShadow = "none";
+    }
+  }
