f0d3361c277801c308a9e0681a738ca596229be0
unknown <tungnguyen22013@gmail.com>
2021-06-23 23:09:21 +0700

    file add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,32 @@
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+
+includeHTML();
 
 function openMobie() {
     var headerPopup=document.querySelector('.header__popup');
@@ -37,32 +66,18 @@ function closeMobie(){
     }
   }
 
-  function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("w3-include-html");
-      if (file) {
-        /* Make an HTTP request using the attribute value as the file name: */
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            /* Remove the attribute, and call this function once more: */
-            elmnt.removeAttribute("w3-include-html");
-            includeHTML();
-          }
-        }
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /* Exit the function: */
-        return;
-      }
-    }
-  }
 
-  includeHTML();
+  var menuSubBtn=document.querySelectorAll('.item__menu i');
+          for(i=0;i<menuSubBtn.length;i++)
+    {     
+      menuSubBtn[i].addEventListener('click',function(){
+          this.classList.toggle('drop_up');
+          var a= this.parentElement;
+          var b=a.getElementsByTagName('ul');
+          for(i=0;i<b.length;i++)
+          {
+              b[i].classList.toggle('max-height350px');
+          }
+      })
+    }
+
